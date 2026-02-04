7f848d4a978e0c3774c9b7202911d9717dee3e23
ibukimu <1480960203@qq.com>
2026-02-04 17:05:45 +0800

    1

--- src/main/java/com/ibuki/controller/DeptController.java
+++ src/main/java/com/ibuki/controller/DeptController.java
@@ -25,18 +25,18 @@ public class DeptController {
         return Result.success(depts);
     }
 
-    @DeleteMapping("/{id}")
-    public Result deleteResultByPath(@PathVariable Integer id) {
-        if (id == null || id <= 0) {
-            log.warn("删除部门失败，id参数无效: " + id);
-            return Result.error("参数不能为空或无效");
-        }
-        int deleted = deptService.deleteResult(id);
-//        System.out.println("删除部门" + id + "成功");
-        log.info("删除部门" + id + "成功");
-        log.info("影响了" + deleted + "行");
-        return Result.success();
-    }
+//    @DeleteMapping("/{id}")
+//    public Result deleteResultByPath(@PathVariable Integer id) {
+//        if (id == null || id <= 0) {
+//            log.warn("删除部门失败，id参数无效: " + id);
+//            return Result.error("参数不能为空或无效");
+//        }
+//        int deleted = deptService.deleteResult(id);
+////        System.out.println("删除部门" + id + "成功");
+//        log.info("删除部门" + id + "成功");
+//        log.info("影响了" + deleted + "行");
+//        return Result.success();
+//    }
     
     @DeleteMapping
     public Result deleteResultByParam(@RequestParam Integer id) {
